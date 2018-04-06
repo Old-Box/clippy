afbcd0386797799a5c5a1f65bf7bdfff4920d7f0
Mats Jalas <mats.jalas@gmail.com>
2018-04-06 14:18:26 +0300

    Add category label and save button

--- src/main/java/org/oldbox/clippy/draganddrop/DrapAndDropFrame.java
+++ src/main/java/org/oldbox/clippy/draganddrop/DrapAndDropFrame.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class DrapAndDropFrame extends JFrame {
     private JTextArea droppableTextArea;
     private JPanel panel;
+    private JButton saveButton;
+    private JLabel categoryLabel;
 
     public DrapAndDropFrame() {
         super();
@@ -14,6 +16,14 @@ public class DrapAndDropFrame extends JFrame {
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
+        this.saveButton.addActionListener(e -> saveButtonPressed());
+
         this.add(this.panel);
+    }
+
+    private void saveButtonPressed() {
+        String content = droppableTextArea.getText();
+        String category = categoryLabel.getText();
+        //TODO: Save data here!
     }
 }
