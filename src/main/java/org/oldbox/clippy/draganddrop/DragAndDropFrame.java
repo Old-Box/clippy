43918a00ed032c499e3924494c9e437bd9936a38
Mats Jalas <mats.jalas@gmail.com>
2018-04-06 15:02:11 +0300

    Add category label setup based on given category

--- src/main/java/org/oldbox/clippy/draganddrop/DragAndDropFrame.java
+++ src/main/java/org/oldbox/clippy/draganddrop/DragAndDropFrame.java
@@ -1,10 +1,12 @@
 package org.oldbox.clippy.draganddrop;
 
+import org.oldbox.clippy.Category;
 import org.oldbox.clippy.ClippyContext;
 import org.oldbox.clippy.ClippyRepository;
 import org.oldbox.clippy.NoteEntry;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class DragAndDropFrame extends JFrame {
     private JTextArea droppableTextArea;
@@ -12,17 +14,30 @@ public class DragAndDropFrame extends JFrame {
     private JButton saveButton;
     private JLabel categoryLabel;
 
-    public DragAndDropFrame() {
+    public DragAndDropFrame(Category category) {
         super();
+        configureFrame();
+
+        updateCategoryLabel(category);
+        this.saveButton.addActionListener(e -> saveButtonPressed());
+
+        this.add(this.panel);
+    }
+
+    private void configureFrame() {
         this.setTitle("Drag and Drop");
         this.setEnabled(true);
         this.setVisible(true);
         this.setSize(300, 300);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
 
-        this.saveButton.addActionListener(e -> saveButtonPressed());
-
-        this.add(this.panel);
+    private void updateCategoryLabel(Category category) {
+        this.categoryLabel.setText(category.getName());
+        if (category.getColor().equals("#FFFFFF")) {
+            this.categoryLabel.setForeground(Color.BLACK);
+        }
+        this.categoryLabel.setBackground(Color.decode(category.getColor()));
     }
 
     private void saveButtonPressed() {
