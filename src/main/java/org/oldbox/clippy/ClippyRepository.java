b13dc8c93ec92e245c5299466915a7ffcff5a1ea
Richard Weber <riche.weber@gmail.com>
2018-04-08 19:12:32 +0300

    Moved from in-memory storage to StorageBackend

--- src/main/java/org/oldbox/clippy/ClippyRepository.java
+++ src/main/java/org/oldbox/clippy/ClippyRepository.java
@@ -1,8 +1,16 @@
 package org.oldbox.clippy;
 
+<<<<<<< HEAD
 import java.awt.*;
+=======
+import org.oldbox.clippy.storage.StorageBackend;
+import org.oldbox.clippy.storage.GsonFileBackend;
+
+>>>>>>> Moved from in-memory storage to StorageBackend
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.nio.file.FileSystemException;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,35 +22,53 @@ public class ClippyRepository {
     public static final int DATABASE_UPDATED = 1337;
 
     private ArrayList<ActionListener> actionListeners;
-    private Map<String, Category> database;
 
-    public ClippyRepository() {
-        this.database = new HashMap<>();
+    private StorageBackend storageBackend;
+
+    public ClippyRepository() throws FileSystemException {
         this.actionListeners = new ArrayList<>();
+        this.storageBackend = new GsonFileBackend();
     }
 
-    public boolean categoryExists(String name) {
-        return this.database.containsKey(name);
+    private Map<String, Category> getDatabase() throws FileSystemException {
+        Map<String, Category> database;
+
+        try {
+            database = storageBackend.loadDatabase();
+        } catch (FileNotFoundException e) {
+            database = new HashMap<String, Category>();
+        }
+
+        return database;
     }
 
-    public void addCategory(String name, Color color) {
+    public boolean categoryExists(String name) throws FileSystemException {
+        return this.getDatabase().containsKey(name);
+    }
+
+    public void addCategory(String name, Color color) throws FileSystemException {
+        Map<String, Category> database = this.getDatabase();
+
         if (this.categoryExists(name)) {
             throw new InvalidParameterException();
         }
-        this.database.put(name, new Category(name, color));
+        database.put(name, new Category(name, color));
+        this.storageBackend.saveDatabase(database);
         this.triggerListeners(DATABASE_UPDATED, "Category added");
     }
 
-    public Category getCategory(String name) {
-        return this.database.get(name);
+    public Category getCategory(String name) throws FileSystemException {
+        return this.getDatabase().get(name);
     }
 
-    public void addEntryToCategory(String categoryName, NoteEntry entry) {
-        this.getCategory(categoryName).getEntries().add(entry);
+    public void addEntryToCategory(String categoryName, NoteEntry entry) throws FileSystemException {
+        Map<String, Category> database = this.getDatabase();
+        database.get(categoryName).getEntries().add(entry);
+        this.storageBackend.saveDatabase(database);
     }
 
-    public Set<String> getCategories() {
-        return this.database.keySet();
+    public Set<String> getCategories() throws FileSystemException {
+        return this.getDatabase().keySet();
     }
 
 
@@ -55,4 +81,5 @@ public class ClippyRepository {
             a.actionPerformed(new ActionEvent(this, code, cmd));
         }
     }
+
 }
