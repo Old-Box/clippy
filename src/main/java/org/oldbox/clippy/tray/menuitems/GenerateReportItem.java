4702a2a96ea6f6a4e1addbec747e2c8c0dfaf56e
Mats Jalas <mats.jalas@gmail.com>
2018-04-11 16:39:47 +0300

    Add write test file logic

--- src/main/java/org/oldbox/clippy/tray/menuitems/GenerateReportItem.java
+++ src/main/java/org/oldbox/clippy/tray/menuitems/GenerateReportItem.java
@@ -2,22 +2,33 @@ package org.oldbox.clippy.tray.menuitems;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
+import java.io.*;
 
 public class GenerateReportItem extends MenuItem {
 
     public GenerateReportItem() {
         super("Generate report");
 
-        this.addActionListener(e -> {
+        this.addActionListener(event -> {
             JFrame frame = new JFrame();
             JFileChooser fileChooser = getSaveFileDialog();
             int result = fileChooser.showSaveDialog(frame);
             if (result == JFileChooser.APPROVE_OPTION) {
                 File report = fileChooser.getSelectedFile();
                 System.out.println("Save report as: " + report.getAbsolutePath());
+                writeReportContentToFile(report.getAbsolutePath(), "Test");
             }
         });
+    }
+
+    private void writeReportContentToFile(String filepath, String content) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filepath));
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private JFileChooser getSaveFileDialog() {
